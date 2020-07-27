f6b615ad9d35e39ac6c89b271037c2a64ae6133f
Xy <50977871+Xyryllium@users.noreply.github.com>
2020-07-27 13:49:31 +0800

    revised portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function toggle() {
+    const sec = document.getElementById('sec');
+    const nav = document.getElementById('navigation');
+    sec.classList.toggle('active');
+    nav.classList.toggle('active');
+
+}
